7d7cf960ec652e2a66772a55defc9316fe406262
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-04-25 21:47:16 +0300

    Show single article

---
+++ app/Http/Resources/ArticleResource.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ArticleResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array|\Illuminate\Contracts\Support\Arrayable|\JsonSerializable
+     */
+    public function toArray($request)
+    {
+        return [
+            'type'          => 'articles',
+            'id'            => (string)$this->id,
+            'attributes'    =>
+            [
+                'title'         => $this->title,
+                'slug'          => $this->slug,
+                'text'          => $this->text,
+                'user_id'       => $this->user_id,
+                'created_at'    => $this->created_at,
+                'updated_at'    => $this->updated_at
+            ],
+        ];
+    }
+}
