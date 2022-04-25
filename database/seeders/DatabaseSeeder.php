9ba14cfa3f5843576bd5ffa21554100b2ae079a0
zno-kharkiv <60223654+BohdanStepanenko@users.noreply.github.com>
2022-04-25 15:01:34 +0300

    Revert "Database"

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -3,9 +3,6 @@
 namespace Database\Seeders;
 
 use Illuminate\Database\Seeder;
-use App\Models\User;
-use App\Models\Article;
-use App\Models\Category;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,35 +13,6 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // Populate 5 users with 10 articles each other
-        User::factory(5)->create()->each(function ($user) {
-            $user->articles()->saveMany(Article::factory(10)->make());
-        });
-
-        // Populate categories
-        $categories = [
-            [
-                'title' => 'PHP',
-            ],
-            [
-                'title' => 'JavaScript',
-            ],
-            [
-                'title' => 'Python',
-            ]
-        ];
-
-        foreach ($categories as $category) {
-            Category::create($category);
-        }
-
-        // Populate pivot table categories to articles
-        $articles = Article::all();
-
-        Category::all()->each(function ($categories) use ($articles) {
-            $categories->articles()->attach(
-                $articles->pluck('id')->toArray()
-            );
-        });
+        // \App\Models\User::factory(10)->create();
     }
 }
