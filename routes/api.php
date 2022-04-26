883ae6dcdf5a06fe608e901dda590fe0b3ce27bb
BohdanStepanenko_HOME <stepanenko1905@gmail.com>
2022-04-27 02:03:33 +0300

    Install jwt

--- routes/api.php
+++ routes/api.php
@@ -14,10 +14,19 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+Route::group([
+
+    'middleware' => 'api',
+    'prefix' => 'auth'
+
+], function ($router) {
+
+    Route::post('login', 'AuthController@login');
+    Route::post('logout', 'AuthController@logout');
+    Route::post('refresh', 'AuthController@refresh');
+    Route::post('me', 'AuthController@me');
+});
+
 Route::resource('articles', 'ArticleController');
 
 Route::resource('categories', 'CategoryController');
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
